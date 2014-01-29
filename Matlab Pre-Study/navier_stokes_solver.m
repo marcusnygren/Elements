9c316cbd39cce417b496da2cd69155a617f48f5e
Gabriel Baravdish <gabba873@student.liu.se>
2014-01-29 17:29:24 +0100

    Changed name from main to navier_stokes_solver, changed parameters and fixed errors. Added return statement for the main function

--- Matlab Pre-Study/navier_stokes_solver.m
+++ Matlab Pre-Study/navier_stokes_solver.m
@@ -1,20 +1,25 @@
-function [ ] = main()
-    
+function [particles] = navier_stokes_solver()
+    clc
     nrFrames = 100;
     nrParticles = 10;
-    timeStep = 10;
+    timeStep = 5;
     gridSizeX = 100;
     gridSizeY = 100;
 
-    velocityTexture = zeros(gridSizeX, gridSizeY, 2);
+    dimension = 2;
+    velocityTexture = zeros(gridSizeX, gridSizeY, dimension);
     preasureTexture = zeros(gridSizeX, gridSizeY);
-    forces = zeros(gridSizeX, gridSizeY);
-    forces((gridSizeX/2 - nrParticles) : (gridSizeX/2 + nrParticles), (gridSizeY/2 - nrParticles) : (gridSizeY/2 + nrParticles)) = 3;
+    
+    forces = zeros(gridSizeX, gridSizeY, dimension);
+    
+    forces((gridSizeX/2 - nrParticles) : (gridSizeX/2 + nrParticles), (gridSizeY/2 - nrParticles) : (gridSizeY/2 + nrParticles),1) = 1;
+    forces((gridSizeX/2 - nrParticles) : (gridSizeX/2 + nrParticles), (gridSizeY/2 - nrParticles) : (gridSizeY/2 + nrParticles),2) = 3;
+
     particles = zeros(gridSizeX, gridSizeY);
     particles((gridSizeX/2 - nrParticles) : (gridSizeX/2 + nrParticles), (gridSizeY/2 - nrParticles) : (gridSizeY/2 + nrParticles)) = 1;
     
     for frames = 1 : nrFrames
-        newVelocityTexture = zeros(gridSizeX, gridSizeY, 2);
+        newVelocityTexture = zeros(gridSizeX, gridSizeY, dimension);
         newPreasureTexture = zeros(gridSizeX, gridSizeY);
         divergenceTexture = zeros(gridSizeX, gridSizeY);
         
@@ -22,10 +27,11 @@ function [ ] = main()
             for y = 10 : (gridSizeY - 10);
                 
                 % Advect velocity.
-                newVelocityTexture(x, y) = calculateAdvection(x, y, timeStep, 1 / gridSizeX, velocityTexture, velocityTexture);
+                newVelocityTexture(x, y) = calculateAdvection(x, y, timeStep, 1 / gridSizeX, velocityTexture, newVelocityTexture);
                 
                 % Advect particles
                 particles(x, y) = calculateAdvection(x, y, timeStep, 1 / gridSizeX, velocityTexture, particles);
+%                 particles(x,y) = 0;
                 
                 % Calculate Viscous Diffusion.
                 
@@ -39,14 +45,14 @@ function [ ] = main()
                 newPreasureTexture(x, y) = calculateJacobiInteration(x, y, -(1/gridSizeX)^2, 4, preasureTexture, divergenceTexture);
                 
                 % Substract pressure gradient.
-                newVelocityTexture(x, y) = gradiantSubtraction(x, y, velocityTexture, preasureTexture, 1/gridSizeX);
+                newVelocityTexture(x, y,:) = gradientSubtraction(x, y, velocityTexture, preasureTexture, 1/gridSizeX);
+                
             end
         end
-        
         velocityTexture = newVelocityTexture;
         preasureTexture = newPreasureTexture;
-        figure
         imshow(particles)
+        drawnow()
 
     end
 end
@@ -70,18 +76,25 @@ end
 
 function [div] = divergence(x, y, w, dx)
 % nabla(dot)w
-    div = (w(x+1,y)-w(x-1,y) + w(x,y+1)-w(x,y-1))*0.5*dx;
+    div = (w(x+1,y,1)-w(x-1,y,1) + w(x,y+1,2)-w(x,y-1,2))*0.5*dx;
+    
 end
 
-function [result] = gradiantSubtraction(x, y, w, p, dx)
-%  du/dt = u(dot)nabla*u - (1/rho)*grad(p) + F
-% OBS dx = 0.5 => 1/2
-% equation 8
+function [result] = gradientSubtraction(x, y, w, p, dx)
+    %  du/dt = u(dot)nabla*u - (1/rho)*grad(p) + F
+    % OBS dx = 0.5 => 1/2
+    % equation 8
+
     uNew(x, y, :) = w(x, y,:);
-    temp = [p(x+1,y) - p(x-1,y); p(x,y+1) - p(x,y-1)]*0.5*dx;
-    size(temp)
-%     uNew(x, y, :) = uNew(x, y, :) - ;
-    result = uNew(x, y, :);
+
+    centralDiff = [p(x+1,y) - p(x-1,y); p(x,y+1) - p(x,y-1)]*0.5*dx;
+    %     size(temp)
+    %     uNew(x, y, :) = uNew(x, y, :) - ;
+    Ux = uNew(x,y,1);
+    Uy = uNew(x,y,2);
+    U = [Ux; Uy];
+    final = U - centralDiff;
+    result = final;
 end
 
 
