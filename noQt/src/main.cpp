65b53d29c88c118594366611a86e24f32eff7200
Snopandan <andersson.nian@gmail.com>
2014-02-23 14:10:45 +0100

    Added simulation loop and framerate counter

--- noQt/src/main.cpp
+++ noQt/src/main.cpp
@@ -23,6 +23,8 @@ Console* Console::theOnlyInstance = NULL;
 std::mutex Console::_instanceMutex;
 std::recursive_mutex Console::_commandItemsMutex;
 
+bool showFps = true;
+
 
 int main( void )
 {
@@ -81,65 +83,86 @@ int main( void )
 		return -1;
 	}
 
+	double _time = glfwGetTime();
+	int nrOfFrames = 0;
+
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-	};
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-
-
-	Simulation test(10,10,10,10);
-	Loader loader;
-	loader.loadPrograms("shaders/programs.txt");
-
-
-	/* Render Loop
-	***************************************************************/
+	Simulation test(100,100,100,0.1);
 	while(isRunning && glfwWindowShouldClose(window) == 0)
 	{
-		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT );
+		if (showFps)
+		{
+			nrOfFrames++;
+			if (glfwGetTime() - _time >= 1)
+			{
+				std::cout << "FPS: " << 1000.f / (float) nrOfFrames << std::endl;
+				nrOfFrames = 0;
+				_time += 1;
+			}
+		}
+		
+		
+		test.stepSimulation();
 
-		// Use our shader
-		glUseProgram(loader.accessProgram("program1"));
-
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
-
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-
-		glDisableVertexAttribArray(0);
-
-		// Swap buffers
-		glfwSwapBuffers(window);
 		glfwPollEvents();
-	} 
+
+	}
+
+	// // Dark blue background
+	// glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	// glViewport(0, 0, 1024, 768);
+
+	// GLuint VertexArrayID;
+	// glGenVertexArrays(1, &VertexArrayID);
+	// glBindVertexArray(VertexArrayID);
+
+	// static const GLfloat g_vertex_buffer_data[] = { 
+	// 	-1.0f, -1.0f, 0.0f,
+	// 	 1.0f, -1.0f, 0.0f,
+	// 	 0.0f,  1.0f, 0.0f,
+	// };
+
+	// GLuint vertexbuffer;
+	// glGenBuffers(1, &vertexbuffer);
+	// glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	// Loader loader;
+	// loader.loadPrograms("shaders/programs.txt");
+
+
+	// /* Render Loop
+	// ***************************************************************/
+	// while(isRunning && glfwWindowShouldClose(window) == 0)
+	// {
+	// 	// Clear the screen
+	// 	glClear( GL_COLOR_BUFFER_BIT );
+
+	// 	// Use our shader
+	// 	glUseProgram(loader.accessProgram("program1"));
+
+	// 	// 1rst attribute buffer : vertices
+	// 	glEnableVertexAttribArray(0);
+	// 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	// 	glVertexAttribPointer(
+	// 		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+	// 		3,                  // size
+	// 		GL_FLOAT,           // type
+	// 		GL_FALSE,           // normalized?
+	// 		0,                  // stride
+	// 		(void*)0            // array buffer offset
+	// 	);
+
+	// 	// Draw the triangle !
+	// 	glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+	// 	glDisableVertexAttribArray(0);
+
+	// 	// Swap buffers
+	// 	glfwSwapBuffers(window);
+	// 	glfwPollEvents();
+	// } 
 
 
 	/* Cleanup
@@ -148,8 +171,8 @@ int main( void )
 	std::cout << "Cleanup" << std::endl;
 	glfwTerminate();
 
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteVertexArrays(1, &VertexArrayID);
+	// glDeleteBuffers(1, &vertexbuffer);
+	// glDeleteVertexArrays(1, &VertexArrayID);
 	
 	if(debug) 
 	{
